d41a3e3c6de97dcd00a8b45af82805d7869fbabf
Dmitriy <dkozlov@northyard.ru>
2018-04-02 18:26:47 +0300

    add tasks 5, 6 with browser installation instruction

---
+++ Task_5-6/src/test/java/AdminLoginFirefoxESR.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class AdminLoginFirefoxESR {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        FirefoxOptions options = new FirefoxOptions()
+                .setLegacy(true) //использовать старую схему запуска (без geckodriver)
+                .setBinary("/opt/firefoxESR/firefox")
+                .setProfile(new FirefoxProfile(new File("/home/user01/.mozilla/firefox/fyq6zl7i.ESR"))); //использовать профиль для ESR
+        driver = new FirefoxDriver(options);
+        System.out.println(((HasCapabilities) driver).getCapabilities()); //вывести настройки веб-драйвера
+
+        driver.navigate().to("http:/localhost/litecart/admin");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void adminLogin() {
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector(".btn")).click();
+        driver.findElement(By.cssSelector(".fa-sign-out")).click();
+    }
+}
---
+++ Task_5-6/src/test/java/AdminLoginNightly.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class AdminLoginNightly {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        FirefoxOptions options = new FirefoxOptions()
+                .setBinary("/opt/firefoxNightly/firefox")
+                .setProfile(new FirefoxProfile(new File("/home/user01/.mozilla/firefox/u5jwunq4.Nightly"))); //использовать профиль для Nightly
+        driver = new FirefoxDriver(options);
+        System.out.println(((HasCapabilities) driver).getCapabilities()); //вывести настройки веб-драйвера
+
+        driver.navigate().to("http:/localhost/litecart/admin");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void adminLogin() {
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.cssSelector(".btn")).click();
+        driver.findElement(By.cssSelector(".fa-sign-out")).click();
+    }
+}
